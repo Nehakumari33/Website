ac43fe01a7a56a7b0d03482ef12475e7c416ad35
neha <nehakumari39584@gmail.com>
2021-02-22 22:50:23 +0530

    Updates

---
+++ precache-manifest.2da2f5bc565e6afc5a4a3b477a31aeb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "46634db8db3079e7530a",
+    "url": "/Website/static/css/main.3eee1851.chunk.css"
+  },
+  {
+    "revision": "46634db8db3079e7530a",
+    "url": "/Website/static/js/main.ad365da1.chunk.js"
+  },
+  {
+    "revision": "36ae524dc1577d9ee8ad",
+    "url": "/Website/static/js/runtime~main.034ab7c4.js"
+  },
+  {
+    "revision": "46293b47b587a759078c",
+    "url": "/Website/static/css/2.43551100.chunk.css"
+  },
+  {
+    "revision": "46293b47b587a759078c",
+    "url": "/Website/static/js/2.336e9ce8.chunk.js"
+  },
+  {
+    "revision": "ca5bd63ed373df91b1da",
+    "url": "/Website/static/js/3.8db08f86.chunk.js"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/Website/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "a18849b815ba0f6c34361bb8c51933a8",
+    "url": "/Website/static/media/th-1.a18849b8.jpg"
+  },
+  {
+    "revision": "b2c19071bb295945da161c57b8bee486",
+    "url": "/Website/index.html"
+  }
+];
